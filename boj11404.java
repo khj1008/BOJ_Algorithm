026ef8b09f6fb4fca55c8e0d59c2aeffbed2ee08
kimhyju <kimhyju703@naver.com>
2019-12-07 16:47:39 +0900

    boj_11404
    Floyd-Warshall

---
+++ boj11404.java
@@ -0,0 +1,78 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class pair implements Comparable{
+	int first;
+	int second;
+	int dist;
+	
+	public pair(int f,int s, int d) {
+		first=f;
+		second=s;
+		dist=d;
+	}
+	static pair of(int f, int s, int d) {
+		return new pair(f, s, d);
+	}
+	@Override
+	public int compareTo(Object obj) {
+		pair tmp=(pair)obj;
+		if(dist>tmp.dist)return 1;
+		else if(dist==tmp.dist)return 0;
+		else return -1;
+	}
+	
+	
+}
+
+
+public class Main {
+	static int N,M;
+	final static int INF=10000000;
+	static int[][] dist=new int[101][101];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		st=new StringTokenizer(br.readLine());
+		N=Integer.parseInt(st.nextToken());
+		M=Integer.parseInt(new StringTokenizer(br.readLine()).nextToken());
+		init();
+		for(int i=0; i<M; i++) {
+			st=new StringTokenizer(br.readLine());
+			int a,b,c;
+			a=Integer.parseInt(st.nextToken());
+			b=Integer.parseInt(st.nextToken());
+			c=Integer.parseInt(st.nextToken());
+			dist[a][b]=Math.min(dist[a][b], c);
+		}
+		
+		for(int k=1; k<=N; k++) {
+			for(int i=1; i<=N; i++) {
+				for(int j=1; j<=N; j++) {
+					dist[i][j]=Math.min(dist[i][j], dist[i][k]+dist[k][j]);
+				}
+			}
+		}
+		for(int i=1; i<=N; i++) {
+			for(int j=1; j<=N; j++) {
+				System.out.print(dist[i][j]+" ");
+			}
+			System.out.println();
+		}
+	
+	}
+	
+	static void init() {
+		for(int i=0; i<=N; i++) {
+			for(int j=0; j<=N; j++) {
+				if(i==j)dist[i][j]=0;
+				else dist[i][j]=INF;
+			}
+		}
+	}
+
+	
+	
+}
